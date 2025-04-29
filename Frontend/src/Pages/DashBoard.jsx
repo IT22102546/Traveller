9ea069fa4e7984bf868995fa189707fa42ef543c
adwx2001 <asankadilshanme@gmail.com>
2025-04-30 01:14:37 +0530

    feat : added the payment functionality

--- Frontend/src/Pages/DashBoard.jsx
+++ Frontend/src/Pages/DashBoard.jsx
@@ -4,40 +4,29 @@ import DashSideBar from "../Components/DashSideBar";
 import DashProfile from "../Components/DashProfile";
 import DashItinary from "../Components/DashItinary";
 import MyPayments from "../Components/MyPayments";
-
-
-
-
-
-
+import FullPaymentRecievedTrips from "../Components/FullPaymentRecievedTrips";
 
 export default function DashBoard() {
   const location = useLocation();
-  const[tab,setTab]= useState();
+  const [tab, setTab] = useState();
 
-  useEffect(()=>{
+  useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
-    const tabFromUrl = urlParams.get('tab');
-    if(tabFromUrl){
-      setTab(tabFromUrl)
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
     }
-  },[location.search]);
-  
+  }, [location.search]);
+
   return (
     <div className="min-h-screen flex flex-col md:flex-row">
       <div className="md:w-56">
-        <DashSideBar/>
+        <DashSideBar />
       </div>
-      {tab==='profile' && <DashProfile/>}
-      {tab==='itinary' && <DashItinary/>}
-      {tab==='mypayments' && <MyPayments/>}
-   
-   
-      
-
-      
-   
-     
+      {tab === "profile" && <DashProfile />}
+      {tab === "itinary" && <DashItinary />}
+      {tab === "mypayments" && <MyPayments />}
+      {tab === "fullpayrecieved" && <FullPaymentRecievedTrips />}
     </div>
-  )
+  );
 }
--- api/routes/order.route.js
+++ api/routes/order.route.js
@@ -1,25 +1,35 @@
-import express from 'express';
-import { verifyToken } from '../utils/verifyUser.js';
+import express from "express";
+import { verifyToken } from "../utils/verifyUser.js";
 import {
   createOrder,
   getOrder,
+  getPendingOrdersWithPaidMembers,
+  getUserPayments,
   updatePaymentStatus,
-} from '../controllers/order.controller.js';
-import multer from 'multer';
+} from "../controllers/order.controller.js";
+import multer from "multer";
 
 const upload = multer();
 
 const router = express.Router();
 
-router.post('/', verifyToken, createOrder);
-router.get('/:orderId', verifyToken, getOrder);
+router.post("/", verifyToken, createOrder);
+router.get("/:orderId", verifyToken, getOrder);
 router.put(
-    '/:orderId/payment', 
-    verifyToken, 
-    upload.single('paymentSlip'),
-    updatePaymentStatus
-  );
-  
+  "/:orderId/payment",
+  verifyToken,
+  upload.single("paymentSlip"),
+  updatePaymentStatus
+);
 
+// this route for track the individuals payment status
+router.get("/:userId/my-payments", getUserPayments);
+
+// get all the order pendings with paid members
+router.get(
+  "/orders/pending-with-paid-members",
+  verifyToken,
+  getPendingOrdersWithPaidMembers
+);
 
 export default router;
